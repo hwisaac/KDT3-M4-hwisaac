50a84e47b5f87866dc6c1e5b0e698f288de3a1bb
Hyeeeein <khi_9866@naver.com>
2022-12-13 21:43:13 +0900

    221213 Add search function

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import style from './Header.module.css';
 
 export default function Header() {
+  const [value, setValue] = useState('');
+
+  const onKeyDown = (event) => {
+    let inputValue = event.target.value;
+    if (event.key === 'Enter' && !event.isComposing) {
+      if (inputValue !== '') {
+        setValue(inputValue.trim());
+      } else {
+        alert('검색어를 입력해주세요');
+      }
+    }
+  };
+
+  // form 으로 보낼 주소 확인
+  const URL = useLocation();
+
   return (
     <header className={style.header}>
       <div className={style.header__top}>
@@ -47,12 +63,17 @@ export default function Header() {
             <div className={style.customerNumber}>관심고객수 117,891</div>
           </div>
           <div className={style.col}>
-            <input type="search" placeholder="검색어를 입력해보세요" />
+            <form action={URL.pathname.includes('search') ? `${value}` : `search/${value}`}>
+              <input onKeyDown={onKeyDown} type="search" placeholder="검색어를 입력해보세요" />
+            </form>
           </div>
         </div>
         <div className={style.mainLogo}>
-          <Link to='/'>
-            <img src="https://shop-phinf.pstatic.net/20191031_66/15725072755036s6lm_PNG/60561378898368862_1948914938.png?type=w640" alt="FRESH MENTOR" />
+          <Link to="/">
+            <img
+              src="https://shop-phinf.pstatic.net/20191031_66/15725072755036s6lm_PNG/60561378898368862_1948914938.png?type=w640"
+              alt="FRESH MENTOR"
+            />
           </Link>
         </div>
       </div>
