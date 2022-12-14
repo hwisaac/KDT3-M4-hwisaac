3155dcf69d47e02393b089cdd72e7840ce63ef8b
Wonny-ing <h.wonny11@gmail.com>
2022-12-14 14:37:16 +0900

    fix: login()OC

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import GNB from './components/gnb/GNB';
 import { Outlet } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // import './reset.css';
 // import './App.css';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <GNB />
 
       <Outlet />
 
       <Footer />
-    </>
+    </QueryClientProvider>
   );
 }
 
