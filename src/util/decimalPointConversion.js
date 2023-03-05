ccbf8cad40c1ad384be1b38cc5dc4554f5f7c10a
Hyeeeein <khi_9866@naver.com>
2023-03-05 16:36:54 +0900

    fix: Fix price decimal converter logic

--- src/util/decimalPointConversion.js
+++ src/util/decimalPointConversion.js
@@ -1,9 +1,7 @@
 export const decimalPointConversion = (price) => {
   price = price.toString();
   let result;
-  if (price.length >= 3) {
-    result = price.split('');
-    price.length === 3 ? result.splice(1, 0, '.') : result.splice(2, 0, '.');
-    return parseFloat(result.join(''));
-  } else return Number(price);
+  result = price.split('');
+  result.splice(-2, 0, '.');
+  return parseFloat(result.join(''));
 };
